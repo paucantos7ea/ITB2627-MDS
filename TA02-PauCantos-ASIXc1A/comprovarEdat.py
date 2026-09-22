96ef1232f2154742939dabc551b7580fff1da27c
paucantos7ea <pau.cantos.7ea@itb.cat>
2026-09-22 12:42:59 +0200

    añadir comprovarEdat.py

---
+++ TA02-PauCantos-ASIXc1A/comprovarEdat.py
@@ -0,0 +1,18 @@
+"""
+
+Pau CAntos
+
+ASIXc 0373 Llenguatges de marques
+
+Descripció: Programa que diu si ets major d'edat o no segons l'edat que introdueixis.
+
+"""
+# Programa que demana l'edad i diu si ets major
+edad = int (input(Quina edat tens?))
+if edad >= 18:
+    print ("Ets major d'edat")
+    else: 
+        if edad < 18:
+            print ("Ets menor d'edat")
+
+print ("Fi del programa")
